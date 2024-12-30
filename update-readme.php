f00683a8ed14a1696db082f3805d827a7d41bdcd
Robin Ingelbrecht <ingebrechtrobin@gmail.com>
2024-12-30 18:54:30 +0100

    Update update-readme.php

--- update-readme.php
+++ update-readme.php
@@ -1,15 +1,5 @@
 <?php
 
-$readme = file_get_contents('README.md');
-$dayTimeSummary = file_get_contents('https://raw.githubusercontent.com/robiningelbrecht/github-commit-history/master/build/markdown/commit-history-day-time-summary.md');
-$weekDaySummary = file_get_contents('https://raw.githubusercontent.com/robiningelbrecht/github-commit-history/master/build/markdown/commit-history-week-day-summary.md');
-$mostRecentCommitsSummary = file_get_contents('https://raw.githubusercontent.com/robiningelbrecht/github-commit-history/master/build/markdown/most-recent-commits.md');
-
-$readme = replaceReadMeSection('commits-per-day-time', $dayTimeSummary, $readme);
-$readme = replaceReadMeSection('commits-per-weekday', $weekDaySummary, $readme);
-$readme = replaceReadMeSection('most-recent-commits', $mostRecentCommitsSummary, $readme);
-file_put_contents('README.md', $readme);
-
 file_put_contents(
     __DIR__.'/assets/medium-blog-posts.svg',
     file_get_contents('https://medium-rss-github.robiningelbrecht.be/@ingelbrechtrobin/0,1,2,3/layout:two-col')
@@ -19,16 +9,3 @@ file_put_contents(
     __DIR__.'/assets/github-streak-stats.svg',
     file_get_contents('https://github-readme-streak-stats.herokuapp.com/?user=robiningelbrecht'),
 );
-
-function replaceReadMeSection(string $sectionName, string $replaceWith, string $subject)
-{
-    return preg_replace(
-        sprintf('/<!--START_SECTION:%s-->[\s\S]+<!--END_SECTION:%s-->/', $sectionName, $sectionName),
-        implode("\n", [
-            sprintf('<!--START_SECTION:%s-->', $sectionName),
-            $replaceWith,
-            sprintf('<!--END_SECTION:%s-->', $sectionName),
-        ]),
-        $subject
-    );
-}
