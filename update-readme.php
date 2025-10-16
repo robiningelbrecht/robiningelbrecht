8026aaffd3d159476f453940a1401a762fbd0ce6
Robin Ingelbrecht <ingebrechtrobin@gmail.com>
2025-10-16 13:56:30 +0200

    Update update-readme.php

--- update-readme.php
+++ update-readme.php
@@ -1,9 +1,9 @@
 <?php
 
-file_put_contents(
+/*file_put_contents(
     __DIR__.'/assets/medium-blog-posts.svg',
     file_get_contents('https://medium-rss-github.robiningelbrecht.be/@ingelbrechtrobin/0,1,2,3/layout:two-col')
-);
+);*/
 
 file_put_contents(
     __DIR__.'/assets/github-streak-stats.svg',
