42681a64fbbb64eb40dbc90d2e34eb680f384a2e
Robin Ingelbrecht <ingebrechtrobin@gmail.com>
2025-12-08 15:11:43 +0100

    Update update-readme.php

--- update-readme.php
+++ update-readme.php
@@ -5,7 +5,7 @@
     file_get_contents('https://medium-rss-github.robiningelbrecht.be/@ingelbrechtrobin/0,1,2,3/layout:two-col')
 );*/
 
-file_put_contents(
+/*file_put_contents(
     __DIR__.'/assets/github-streak-stats.svg',
     file_get_contents('https://github-readme-stats.vercel.app/api?username=robiningelbrecht&show_icons=true'),
-);
+);*/
